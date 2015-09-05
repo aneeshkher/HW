9565881497f954548aa31e5525f7130d3aff0dfb
aneeshkher <aneeshkher@gmail.com>
2015-09-05 17:41:05 -0400

    Testing post commit

--- test.pl
+++ test.pl
@@ -1,9 +1,10 @@
+#!C:\Dwimperl\perl\bin\perl.exe -w
 @output = `git remote -v`;
 
 #print $output;
 $output[0] =~ m/origin\s+(\S+)\s+/;
 $repo = $1;
-print $1."\n";
+#print $1."\n";
 
 @command = "start chrome $repo";
 system (@command);
