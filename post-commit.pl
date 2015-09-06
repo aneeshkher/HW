949825ba424ee42c6fb80d5094038a9184850775
aneeshkher <aneeshkher@gmail.com>
2015-09-06 12:32:39 -0400

    Add post commit file

--- post-commit.pl
+++ post-commit.pl
@@ -1,11 +1,8 @@
 #!C:\Dwimperl\perl\bin\perl.exe -w
 @output = `git remote -v`;
 
-#print $output;
 $output[0] =~ m/origin\s+(\S+)\s+/;
 $repo = $1;
-print $1."\n";
 
-#print "Test line for adding some change\n";
 @command = "start chrome $repo";
 system (@command);
